8069a41a39da4bf7acaf93055814eecb8ca951cd
fans656 <fans656@yahoo.com>
2017-09-01 23:33:08 +0800

    devlog

--- src/kernel/0kernel.c
+++ src/kernel/0kernel.c
@@ -9,13 +9,7 @@ void kernel_entry() {
     setup_idt();
     clear_screen();
     
-    printf("%p\n", key_states);
-    for (int i = 0; i < 128; ++i) {
-        printf("%d ", key_states[i]);
-        if (i % 8 == 7) {
-            put_char('\n');
-        }
-    }
+    snake_game();
 
     hlt();
 }
